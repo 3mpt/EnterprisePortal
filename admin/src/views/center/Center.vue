3d188710e7ce1586440e0c4ae4fb722c0491dad8
zhangyucong <zhangyucong@duiba.com.cn>
2024-07-05 18:25:28 +0800

    feat: 周五提交

--- admin/src/views/center/Center.vue
+++ admin/src/views/center/Center.vue
@@ -1,20 +1,27 @@
 <template>
-    <div>个人中心</div>
+  <div>
+    <el-page-header content="个人中心" icon="" title="企业门户管理系统" />
+    <el-row :gutter="20">
+      <el-col :span="8">
+        <el-card class="box-card">
+          <el-avatar :size="100" :src="avatarUrl" /> 
+          <h3>{{ store.state.userInfo.username }}</h3>
+          <h3>{{ store.state.userInfo.role===1?'管理员':'编辑' }}</h3>
+
+          </el-card></el-col>
+      <el-col :span="16">2</el-col>
+    </el-row>
+  </div>
 </template>
 
-<script>
-export default {
-    components: {},
-    props: {},
-    data() {
-        return {
-        };
-    },
-    watch: {},
-    computed: {},
-    methods: {},
-    created() { },
-    mounted() { }
-};
+<script setup>
+import { useStore } from 'vuex';
+import { reactive, ref, computed } from 'vue';
+const store = useStore();
+const avatarUrl = computed(() =>
+  store.state.userInfo.avatar
+    ? 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png'
+    : 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
+);
 </script>
 <style lang="scss" scoped></style>
--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -7,7 +7,7 @@ const UserType = {
     gender: Number, //0:男 1:女
     introduction: String, //个人简介
     avatar: String,  //头像
-    role: String //用户角色 管理员1 编辑2
+    role: Number //用户角色 管理员1 编辑2
 }
 const UserModel = mongoose.model("user", new Schema(UserType))
 module.exports = UserModel;
